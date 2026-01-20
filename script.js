85c009963b3916787857d740cf619e17576c2ca0
monukumarchaudharigit config --global user.nemail monukumarchaudhari204@gmail.comgit conig listgit config --global user.name monukumarchaudhari <monukumarchaudhaari204@gmail.com>
2026-01-20 16:02:48 +0530

    jsAdd

---
+++ script.js
@@ -0,0 +1,12 @@
+//
+
+let a = 23
+if ( a <= 12){
+    console.log("child")
+} else if ( a <= 18){
+    console.log("Kishor")
+}else if (a<=21){
+    console("minner")
+} else{
+    console.log("older")
+}
